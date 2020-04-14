8ef9dfb820f82fd558ac8d908d6416c1b0469d86
theatredu1k <63430690+theatredu1k@users.noreply.github.com>
2020-04-14 17:17:01 +0100

    update from local Atom

--- computerVision/faceTracker/eye/draw.js
+++ computerVision/faceTracker/eye/draw.js
@@ -1,12 +1,14 @@
-//https://gist.github.com/kerryrodden/6eb0998a8aadf9cc63b1445dd1897aab
+//based on https://gist.github.com/kerryrodden/6eb0998a8aadf9cc63b1445dd1897aab
+
+
 let capture = null;
 let tracker = null;
 let positions = null;
 let w = 0, h = 0;
 
 function setup() {
-  w = windowWidth;
-  h = windowHeight;
+  w = 640;
+  h = 480;
   capture = createCapture(VIDEO);
   createCanvas(w, h);
   capture.size(w, h);
@@ -20,13 +22,13 @@ function setup() {
   tracker.init();
   tracker.start(capture.elt);
 }
-
+//=====================DRAW function=====================================
 function draw() {
   // Flip the canvas so that we get a mirror image
-	translate(w, 0);
+  translate(w, 0);
   scale(-1.0, 1.0);
   // Uncomment the line below to see the webcam image (and no trail)
-  //image(capture, 0, 0, w, h);
+ // image(capture, 0, 0, w, h);
   positions = tracker.getCurrentPosition();
 
   if (positions.length > 0) {
@@ -35,22 +37,28 @@ function draw() {
     // https://www.auduno.com/clmtrackr/docs/reference.html
     const eye1 = {
       outline: [23, 63, 24, 64, 25, 65, 26, 66].map(getPoint),
+      onePoint: getPoint(23),
+      twoPoint: getPoint(25),
       center: getPoint(27),
       top: getPoint(24),
       bottom: getPoint(26)
     };
     const eye2 = {
       outline: [28, 67, 29, 68, 30, 69, 31, 70].map(getPoint),
+      onePoint: getPoint(28),
+      twoPoint: getPoint(30),
       center: getPoint(32),
       top: getPoint(29),
       bottom: getPoint(31)
     }
-    
+
     const irisColor = color(random(360), 80, 80, 0.4);
     drawEye(eye1, irisColor);
-		drawEye(eye2, irisColor);
+  //  drawEye(eye2, irisColor);
   }
-}
+}//end draw
+
+//==============other Functions===============================
 
 function getPoint(index) {
   return createVector(positions[index][0], positions[index][1]);
@@ -60,44 +68,65 @@ function drawEye(eye, irisColor) {
   noFill();
   stroke(255, 0.4);
   drawEyeOutline(eye);
-  
+
   const irisRadius = min(eye.center.dist(eye.top), eye.center.dist(eye.bottom));
-  const irisSize = irisRadius * 2;
+  const irisSize = irisRadius*2;
   noStroke();
   fill(irisColor);
   ellipse(eye.center.x, eye.center.y, irisSize, irisSize);
-  
+  //point(eye.center.x,eye.center.y);
   const pupilSize = irisSize / 3;
-  fill(0, 0.6);
-  ellipse(eye.center.x, eye.center.y, pupilSize, pupilSize);
+//   fill(0, 0.6);// black pupil
+//   ellipse(eye.center.x, eye.center.y, pupilSize, pupilSize);
 }
 
 function drawEyeOutline(eye) {
 	beginShape();
   const firstPoint = eye.outline[0];
+
+
   eye.outline.forEach((p, i) => {
-    curveVertex(p.x, p.y);
-    if (i === 0) {
-      // Duplicate the initial point (see curveVertex documentation)
-      curveVertex(firstPoint.x, firstPoint.y);
-    }
-    if (i === eye.outline.length - 1) {
-      // Close the curve and duplicate the closing point
-      curveVertex(firstPoint.x, firstPoint.y);
-      curveVertex(firstPoint.x, firstPoint.y);
-    }
+  //  curveVertex(p.x, p.y);
+
+ //   if (random(80)<5) {
+ //     // translate(0,20);
+ //     // rotate(random(60)*i/180);
+ //     //line(eye.onePoint.x,eye.onePoint.y,eye.twoPoint.x,eye.twoPoint.y);
+ //   }else{
+ //     point(eye.bottom.x,eye.bottom.y);
+ //     point(eye.onePoint.x,eye.onePoint.y);
+ // }
+ //
+
+    // if (i === 0) {
+    //   // Duplicate the initial point (see curveVertex documentation)
+    //   curveVertex(firstPoint.x, firstPoint.y);
+    // }
+    // if (i === eye.outline.length - 1) {
+    //   // Close the curve and duplicate the closing point
+    //   curveVertex(firstPoint.x, firstPoint.y);
+    //   curveVertex(firstPoint.x, firstPoint.y);
+    //
+    // }
   });
   endShape();
+
 }
 
 function keyPressed() {
   // Clear background
-  background(0);
+
+	if (keyCode === 27) {
+		background(0);
+	}
+	if (keyCode === 80) {
+		const timestamp = timestampString();
+  		saveCanvas("eyeTrail-" + timestamp, "png");
+	}
 }
 
 function mouseClicked() {
-  const timestamp = timestampString();
-  saveCanvas("eyeTrail-" + timestamp, "png");
+
 }
 
 function timestampString() {
@@ -110,3 +139,7 @@ function windowResized() {
   resizeCanvas(w, h);
   background(0);
 }
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
