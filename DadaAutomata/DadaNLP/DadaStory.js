48824388ecb06975cd05b7e1a21c1c61ebc13bc4
sreco002 <sreco002@gmail.com>
2020-10-12 23:32:14 +0100

    let {PythonShell} = require('python-shell')

--- DadaAutomata/DadaNLP/DadaStory.js
+++ DadaAutomata/DadaNLP/DadaStory.js
@@ -1,8 +1,9 @@
 
 var myPythonScriptPath = 'DadaStory.py';
 // Use python shell
-// var PythonShell = require('python-shell');
-import PythonShell from 'python-shell';
+ // var PythonShell = require('python-shell');
+// import PythonShell from 'python-shell';
+let {PythonShell} = require('python-shell')
 var pyshell = new PythonShell(myPythonScriptPath);
 
 pyshell.on('message', function (message) {
