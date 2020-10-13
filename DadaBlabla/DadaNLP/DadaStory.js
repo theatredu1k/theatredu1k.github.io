3f9ab90b59ea77178fac770632c7b546f026d553
sreco002 <sreco002@gmail.com>
2020-10-13 23:30:00 +0100

    unexpected {}); line8

--- DadaBlabla/DadaNLP/DadaStory.js
+++ DadaBlabla/DadaNLP/DadaStory.js
@@ -4,8 +4,7 @@ let myPythonScriptPath = 'DadaStory.py';
 let PythonShell = require('python-shell');
 // import PythonShell from 'python-shell';
 
-// let {PythonShell} = require(['python-shell'], function (python-shell) {
-});
+// let {PythonShell} = require(['python-shell'], function (python-shell) {});
 var pyshell = new PythonShell(myPythonScriptPath);
 
 pyshell.on('message', function (message) {
