4e1addc89ce8b4dc9f16ec3900c059b650c8ceb3
Theatredu1k-Digital <63430690+theatredu1k@users.noreply.github.com>
2021-03-02 13:03:55 +0000

    Update wiz_sorting.js

--- DadaBlabla/DadaWiz/wiz_sorting.js
+++ DadaBlabla/DadaWiz/wiz_sorting.js
@@ -1,4 +1,8 @@
-//https://www.youtube.com/watch?v=uNQSVU0IKec&list=PLRqwX-V7Uu6YrbSJBg32eTzUU50E2B8Ch&index=3
+// Shiffman : https://www.youtube.com/watch?v=uNQSVU0IKec
+// plan a manipulation with text to manipulate a square pixel on screen
+//create a textfield to enter a text 
+//..... connect sentiment analysis 
+// map sentiment analysis to a movement in a square
 
 let textfield;
 let output;
@@ -6,7 +10,7 @@ let submit;
 
 function setup(){
   noCanvas();
-  //textfield = createInput(" sorting out stuff in a text");
+  textfield = createInput(" sorting out stuff in a text");
   textfield = select('#mot');
   //textfield.changed(newText);// event entering the text in the field
   //textfield.input(newTyping);// event at every typing
