aab77b87413437e9dea3f5995bdefc5577d0e874
sreco002 <sreco002@gmail.com>
2021-03-12 20:40:49 +0000

    var button erase

--- DadaBlabla/DadaNLP/DadaDiary.js
+++ DadaBlabla/DadaNLP/DadaDiary.js
@@ -35,7 +35,6 @@ function setup() {
   createCanvas(320,240);
   createFileInput(fileSelected);
   paragraph=createP('©Theatredu1k '+day()+month()+year());
-  // var button = select('loadFile');
   button = createButton('loadFile');
   button.mousePressed(loadFile);
 
