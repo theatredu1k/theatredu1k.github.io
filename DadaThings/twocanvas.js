76298899968c1e9b25ba7c505abda634b75880da
sreco002 <sreco002@gmail.com>
2021-03-13 18:47:14 +0000

    p. devant toutes les functions et variables except celles dewclarees dans la fonction

--- DadaThings/twocanvas.js
+++ DadaThings/twocanvas.js
@@ -1,20 +1,4 @@
 // Sketch Timer
-// var m = function( p ) { // p could be any variable name
-//   var x = 100;
-//   var y = 100;
-//   p.setup = function() {
-//     p.createCanvas(400, 200);
-//   };
-//
-//   p.draw = function() {
-//     p.background(0);
-//     p.fill(255);
-//     p.rect(x,y,50,50);
-//   };
-// };
-
-
-
 
 
 //---------------------------------------------------------------
@@ -117,9 +101,10 @@ p.setup()= function()  {
 	};
 };// end p setup timer
 }
+var myp5 = new p5(m, 'c3');
 //---------------------------------------------------------------
-// Sketch One
-var myp5 = new p5(s, 'c1');
+
+
 
 // Sketch One
 var s = function( p ) { // p could be any variable name
