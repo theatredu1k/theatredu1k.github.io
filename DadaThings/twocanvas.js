2e59540cb5fc41f63d33ca4366ac80d4e0b3f9d8
sreco002 <sreco002@gmail.com>
2021-03-14 18:30:50 +0000

    p.floor

--- DadaThings/twocanvas.js
+++ DadaThings/twocanvas.js
@@ -16,7 +16,7 @@ var s = function( p ) { // p could be any variable name
   // Convert seconds to min:sec
   p.convertSeconds = function(s) {
     this.s = s;
-    let min = floor(this.s / 60);
+    let min = p.floor(this.s / 60);
     let sec = this.s % 60;
     return p.nf(min, 2) + ':' + p.nf(sec, 2);
   };
