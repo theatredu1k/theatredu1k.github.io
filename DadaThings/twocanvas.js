c514962079f0127045ca8494b5790c7cc18ec566
sreco002 <sreco002@gmail.com>
2021-03-14 22:14:48 +0000

    test

--- DadaThings/twocanvas.js
+++ DadaThings/twocanvas.js
@@ -52,10 +52,10 @@ var s = function( p ) { // p could be any variable name
 
   p.draw = function() {
     timeleft--;
-    // Process URL ?minute=
+    document.getElementById("mytimer").innerHTML= timeleft;
 
-    result = p.convertSeconds(timeleft);// this is working with p.convertseconds and p. before all the functions inside convertseconds
-    document.getElementById("mytimer").innerHTML= result;
+    // result = p.convertSeconds(timeleft);// this is working with p.convertseconds and p. before all the functions inside convertseconds
+    // document.getElementById("mytimer").innerHTML= result;
 
     p.fill(255);
     p.rect(x,y,50,50);
