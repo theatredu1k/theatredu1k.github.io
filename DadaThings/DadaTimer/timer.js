0901abde68ed88e85e71c948d47f556408aa2939
sreco002 <sreco002@gmail.com>
2021-03-15 21:32:49 +0000

    var myp5 = new p5(t, 'c2');

--- DadaThings/DadaTimer/timer.js
+++ DadaThings/DadaTimer/timer.js
@@ -125,3 +125,25 @@ var s = function( p ) { // p could be any variable name
   };
 };// end var s function(p)
 var myp5 = new p5(s, 'c1');// first canvas c1 from the bottom
+
+// Sketch Two
+var t = function( p ) { // p could be any variable name
+  var x = 100.0;// variables dans cette fonction
+  var y = 100;
+  var speed = 2.5;
+  p.setup = function() {
+    p.createCanvas(400, 200);
+  };
+
+  p.draw = function() {
+    p.background(100);// background de cette fonction
+    p.fill(1);
+    x += speed;
+    if(x > p.width){
+      x = 0;
+    }
+    p.ellipse(x,y,50,50);
+
+  };
+};
+var myp5 = new p5(t, 'c2');
